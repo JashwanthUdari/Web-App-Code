141be1c1a5909ac14d226f0b3fd69aeac93c4dce
Jashwanth Udari <jashwanthudari@hotmail.com>
2025-11-26 15:56:00 +0530

    Added Node.js app and Dockerfile

---
+++ server.js
@@ -0,0 +1,9 @@
+﻿const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.send('Hello from GitHub Actions CI Demo!');
+});
+app.listen(PORT, () => {
+  console.log(Server running on port );
+});
